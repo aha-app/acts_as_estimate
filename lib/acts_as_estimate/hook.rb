48fc83f35ff970f8d1521a8e727d95009ab6a2be
k1w1 <k1w1@k1w1.org>
2013-09-10 14:42:15 -0700

    * Adding methods for reading/writing.

--- lib/acts_as_estimate/hook.rb
+++ lib/acts_as_estimate/hook.rb
@@ -2,6 +2,7 @@ module ActsAsEstimate::Hook
   def acts_as_estimate(*args)
     field_name = args[0].to_sym
     options = args.extract_options!
+    
     include ActsAsEstimate::InstanceMethods
     
     class_eval <<-EOV
@@ -13,10 +14,5 @@ module ActsAsEstimate::Hook
         write_estimate_field(field_name, new_value)
       end
     EOV
-  
-    
-    p " => instace methods added"
-    before_filter :an_awesome_filter
-    p " => filter added"
   end
 end
