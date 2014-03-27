e0038fd9f6bce0ea08f2b7cdb8f6545ee9c31e71
k1w1 <k1w1@k1w1.org>
2014-03-26 19:43:53 -0700

    * Fixed parsing of fractional times.

--- lib/acts_as_estimate/instance_methods.rb
+++ lib/acts_as_estimate/instance_methods.rb
@@ -13,11 +13,7 @@ module ActsAsEstimate::InstanceMethods
         # If only a number is entered, assume units are days.
         new_value = "#{new_value}d"
       end
-      if new_value.to_i == 0
-        duration = 0
-      else
-        duration = ChronicDuration.parse(new_value)
-      end
+      duration = ChronicDuration.parse(new_value) || 0
       send("#{field_name}=", duration / 60) if duration
     end
   end
